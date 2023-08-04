06bd829141877d669fccd1076bdd244ea693ee3f
Don Christensen <noobgrammer@gmail.com>
2023-08-04 04:23:40 -0700

    switched function name

--- 8kyu/cantSleepCountSheep.js
+++ 8kyu/cantSleepCountSheep.js
@@ -3,11 +3,11 @@
 
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 
-function counting(num) {
+function countSheep(num) {
   let sheep = "";
   for (let i = 0; i < num; i++) {
     sheep += `${i + 1} sheep...`;
   }
   return sheep;
 }
-console.log(counting(3));
+console.log(countSheep(3));
