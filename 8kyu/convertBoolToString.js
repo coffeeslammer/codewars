4f585c8f23abb7ee20c23bc1344d04a933bc3c98
Don Christensen <noobgrammer@gmail.com>
2023-08-06 07:59:35 -0700

    8kyu convertBoolToString.js yes/no version

---
+++ 8kyu/convertBoolToString.js
@@ -0,0 +1,6 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function convert(value){
+    return value?"Yes":"No"
+}
+console.log(convert(true));
