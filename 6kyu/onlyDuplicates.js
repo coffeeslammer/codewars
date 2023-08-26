2894671e5c7a17449f23363b69734f70a5f20c28
Don Christensen <noobgrammer@gmail.com>
2023-08-26 12:27:27 -0700

    6kyu onlyDuplicates.js

---
+++ 6kyu/onlyDuplicates.js
@@ -0,0 +1,28 @@
+// Given a string, remove any characters that are unique from the string.
+
+// Example:
+
+// input: "abccdefee"
+
+// output: "cceee"
+
+function duplicates(str) {
+  const myArray = str.split("");
+  const set = new Set(myArray);
+  return myArray
+    .filter((item, i) => {
+      if (set.has(item)) {
+        set.delete(item);
+        if (str.lastIndexOf(item) !== i) return item;
+      } else {
+        return item;
+      }
+    })
+    .join("");
+}
+console.log(duplicates("colloquial"));
+
+// Test.assertEquals(onlyDuplicates("abccdefee"), "cceee");
+// Test.assertEquals(onlyDuplicates("hello"), "ll");
+// Test.assertEquals(onlyDuplicates("colloquial"), "ollol");
+// Test.assertEquals(onlyDuplicates("foundersandcoders"), "ondersndoders");
