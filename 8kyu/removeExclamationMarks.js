96e53ba311673debe3d568ac2add0983ab67d7fa
Don Christensen <noobgrammer@gmail.com>
2023-09-01 05:34:58 -0700

    cleaned up

--- 8kyu/removeExclamationMarks.js
+++ 8kyu/removeExclamationMarks.js
@@ -1,6 +1,6 @@
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
-function removing(str) {
-  return str.replaceAll("!", "");
+function removeExclamationMarks(s) {
+  return s.replaceAll("!", "");
 }
-console.log(removing("faslk;f;a! afjda !  ! !Jl!"));
+console.log(removeExclamationMarks("faslk;f;a! afjda !  ! !Jl!"));
