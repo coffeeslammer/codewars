7f94ea18a1b5e6f4fcf4e861a2ccaf6d5824ba7a
Don Christensen <noobgrammer@gmail.com>
2023-10-30 16:56:52 -0700

    7kyu findTheCapital.js

---
+++ 7kyu/findTheCapital.js
@@ -0,0 +1,15 @@
+// Instructions
+
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+// Example (Input --> Output)
+
+// "CodEWaRs" --> [0,3,4,6]
+
+function findCapital(word) {
+  const ary = [];
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === word[i].toUpperCase()) ary.push(i);
+  }
+  return ary;
+}
+console.log(findCapital("CodEWaRs"));
